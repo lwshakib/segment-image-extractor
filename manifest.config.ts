726464a2b76ce470aea287ca33267a85f3a5b20e
lwshakib <leadwithshakib@gmail.com>
2025-08-15 08:28:56 +0600

    RELEASE 7.0

--- manifest.config.ts
+++ manifest.config.ts
@@ -1,28 +1,26 @@
-import { defineManifest } from '@crxjs/vite-plugin'
-import pkg from './package.json'
+import { defineManifest } from "@crxjs/vite-plugin";
+import pkg from "./package.json";
 
 export default defineManifest({
   manifest_version: 3,
-  name: pkg.name,
+  name: pkg.extension_name,
+  description: pkg.extension_description,
   version: pkg.version,
   icons: {
-    48: 'public/logo.png',
+    16: "public/icon16.png",
+    32: "public/icon32.png",
+    48: "public/icon48.png",
+    128: "public/icon128.png",
   },
+  permissions: ["sidePanel", "activeTab", "scripting"],
+  host_permissions: ["<all_urls>"],
   action: {
-    default_icon: {
-      48: 'public/logo.png',
-    },
-    default_popup: 'src/popup/index.html',
+    default_title: pkg.extension_name,
   },
-  permissions: [
-    'sidePanel',
-    'contentSettings',
-  ],
-  content_scripts: [{
-    js: ['src/content/main.tsx'],
-    matches: ['https://*/*'],
-  }],
+  background: {
+    service_worker: "src/background.ts",
+  },
   side_panel: {
-    default_path: 'src/sidepanel/index.html',
+    default_path: "src/sidepanel/index.html",
   },
-})
+});
