ff93bc653f352da095a61728a01c0f5eafc17da2
lwshakib <leadwithshakib@gmail.com>
2025-08-15 08:26:31 +0600

    RELEASE 3.0

--- src/sidepanel/main.tsx
+++ src/sidepanel/main.tsx
@@ -2,9 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { ThemeProvider } from '@/components/theme-provider.tsx'
+import { Toaster } from '@/components/ui/sonner.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <App />
+      <Toaster />
+    </ThemeProvider>
   </StrictMode>,
 )
